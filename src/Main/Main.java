b5d5e5bf98b4fe6c2db3195f7bae2e6d4235b1f4
ABHISHEK <abhishek.yadav_cs18@gla.ac.in>
2020-02-27 18:29:20 +0530

    duplicacy in huffman code

---
+++ src/Main/Main.java
@@ -0,0 +1,29 @@
+package Main;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string ");
+        String userInput = scanner.next();
+        String[] huffmanCodes = {"1010","100000","00000","10110","010","110011","100010","0001","0110","110001011","11010011","10111","110010","0111","1001","100001","1100001001","0010","0011","1101","00001","1100000","1100010","1100001000","100011","1100001010"};
+        String lowerCaseString  = userInput.toLowerCase();
+        char[] characterArray = lowerCaseString.toCharArray();
+        for (int i = 0; i <characterArray.length ; i++)
+        {
+            int ascii = characterArray[i];
+            ascii= ascii-97;
+
+            System.out.print(huffmanCodes[ascii]+" ");
+
+
+        }
+        scanner.close();
+           
+
+
+    }
+}
